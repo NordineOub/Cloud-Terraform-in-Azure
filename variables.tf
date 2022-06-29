aa7a0e1e9778df280de2b4b1ca51e38da7289f7c
NordineOub <75072085+NordineOub@users.noreply.github.com>
2022-06-29 08:34:05 +0200

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "name_client" {
+  default       = "devops-20200174" 
+  description   = "Nom du client"
+}
+
+variable "resource_group_name" {
+  default       = "devops-TP2"
+  description   = "Name of the resource group."
+}
+
+variable "location" {
+  default       = "francecentral"
+  description   = "Location of the resource group."
+}
